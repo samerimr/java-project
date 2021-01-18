e1c4b697bbec5627e016736b95713c437601085e
samerimr <imraish.09@gmail.com>
2021-01-18 17:03:30 +0200

    phase1

---
+++ src/main/java/com/axsosplatform/AxsosPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.axsosplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxsosPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxsosPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/axsosplatform/AxsosPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axsosplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxsosPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
